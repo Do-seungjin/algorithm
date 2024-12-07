df6a6c56272711ec9638495c23cdec5e179247eb
Do_SeungJin <dosjin01@gmail.com>
2024-12-07 20:23:20 +0900

    [도승진] 단어뒤집기 - 직접

--- algorithm/src/algorithm_day1/단어뒤집기.java
+++ algorithm/src/algorithm_day1/단어뒤집기.java
@@ -6,11 +6,24 @@ import java.util.Scanner;
 public class 단어뒤집기 {
 	public ArrayList<String> solution(int n, String[] str){
 		ArrayList<String> answer = new ArrayList<>();
-		for(int i=0 ; i<str.length ; i++) {
-			String tmp = new StringBuilder(str[i]).reverse().toString();
+		/*reverse를 사용한 뒤집기
+		 * for(int i=0 ; i<str.length ; i++) { String tmp = new
+		 * StringBuilder(str[i]).reverse().toString(); answer.add(tmp); }
+		 */
+		for(String x : str) {
+			char[] s = x.toCharArray();
+			int lt = 0;
+			int rt = x.length()-1;
+			while(lt<rt) {
+				char tmp = s[lt];
+				s[lt]=s[rt];
+				s[rt]=tmp;
+				lt++;
+				rt--;
+			}
+			String tmp = String.valueOf(s);
 			answer.add(tmp);
 		}
-		
 		return answer;
 	}
 	public static void main(String[] args) {
