3b6d297b4be4c5c040fde0606ceb2be363b909c0
littl <littl@192.168.219.107>
2024-12-09 23:48:00 +0900

    [도승진]숫자만추출

---
+++ algorithm/src/algorithm_day1/숫자만추출.java
@@ -0,0 +1,22 @@
+package algorithm_day1;
+
+import java.util.Scanner;
+
+public class 숫자만추출 {
+	public int solution(String s) {
+		String answer = "";
+		for(char x : s.toCharArray()) {
+			if(Character.isDigit(x)) {
+				answer += x;
+			}
+		}
+		return Integer.parseInt(answer);
+	}
+	public static void main(String[] args) {
+		숫자만추출 T = new 숫자만추출();
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		System.out.println(T.solution(str));
+	}
+	
+}
